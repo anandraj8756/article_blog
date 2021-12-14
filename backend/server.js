e446dfe95b9e2fd2ca0b80381e088a48830bc054
Anand Raj <anandallen95@gmail.com>
2021-12-14 22:41:54 +0530

    back server run

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+require('dotenv').config
+
+
+//APP
+const app = express()
+
+//middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+//cors
+app.use(cors())
+
+//routes
+app.get('/api', (req, res) => {
+    res.json({time: Date().toString()})
+
+})
+
+//PORT
+const port = process.env.PORT || 8000
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+});
+
